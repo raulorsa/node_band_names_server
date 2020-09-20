e773b809098ef4c81ea8044ab72f3c5ab1393f42
Raúl Ortega <raul.ortega.s@gmail.com>
2020-09-20 02:23:33 +0200

    conectado y controller

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ require('./database/config').dbConnection();
 //App de Express
 const app = express();
 
+app.use (express.json());
+
 //Node server
 const server = require('http').createServer(app);
 module.exports.io = require('socket.io')(server);
@@ -20,6 +22,9 @@ require('./sockets/socket');
 // Path público
 const publicPath = path.resolve( __dirname, 'public' );
 app.use( express.static( publicPath ) );
+
+//Mis Rutas
+app.use('/api/login', require('./routes/auth'));
 
 
 // app.listen(3000, (err) =>{
---
+++ routes/auth.js
@@ -0,0 +1,10 @@
+const {Router} = require('express');
+const { crearUsuario } = require('../controllers/auth');
+
+const router = Router();
+
+
+router.post('/new', crearUsuario);
+
+
+module.exports = router;
